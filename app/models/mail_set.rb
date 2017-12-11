2876809da3be129c51b46afe6f8a6bb8db198399
rmnlknv <snovix@gmail.com>
2017-12-11 06:14:56 +0100

    Added regex verification of email

--- app/models/mail_set.rb
+++ app/models/mail_set.rb
@@ -3,6 +3,7 @@ class MailSet < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { in: 3..24 }
+
   before_save :format_addressee
 
   private
@@ -10,5 +11,6 @@ class MailSet < ApplicationRecord
   def format_addressee
     # removes spaces that are more than 1 in a row, new lines and tabulations
     self.addressee = self.addressee.gsub(/[\r\n\t]/, ' ').gsub(/ {2,}/, ' ')
+    self.addressee = self.addressee.split.select{ |a| a[/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i] }.uniq.join(' ')
   end
 end
