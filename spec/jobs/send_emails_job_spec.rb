604e1245aa1322781129201417a801a477c15819
rmnlknv <snovix@gmail.com>
2017-12-13 14:29:31 +0100

    Added test for job spec

--- spec/jobs/send_emails_job_spec.rb
+++ spec/jobs/send_emails_job_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SendEmailsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "matches with enqueued job" do
+    ActiveJob::Base.queue_adapter = :test
+    expect {
+      SendEmailsJob.perform_later
+    }.to have_enqueued_job(SendEmailsJob)
+  end
 end
